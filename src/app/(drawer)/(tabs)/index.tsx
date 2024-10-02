9731cd3b2e82986752b7aa3fa0a0b05b5e78c118
arthur <arthur.rios007@gmail.com>
2024-10-01 22:06:25 -0300

    Rotas aninhadas - Adicionando mais Stack

--- src/app/(drawer)/(tabs)/index.tsx
+++ src/app/(drawer)/(tabs)/index.tsx
@@ -1,9 +1,15 @@
+import { router } from 'expo-router'
 import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function Products() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Lista de Produtos</Text>
+
+      <TouchableOpacity onPress={() => router.navigate('/product/52')}>
+        <Text>Abrir produto</Text>
+      </TouchableOpacity>
     </View>
   )
 }
